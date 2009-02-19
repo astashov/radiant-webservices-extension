28f002c63643af404c51e297de25ae519683ac91
Anton Astashov <anton@localhost.(none)>
2009-02-19 13:58:52 +0700

    Removed tag attribute 'route_handler_params' - now it will load all route handler parameters automatically and use only necessary

--- app/models/webservice_tags.rb
+++ app/models/webservice_tags.rb
@@ -4,12 +4,8 @@ module WebserviceTags
   tag 'webservice' do |tag|
     webservice = Webservice.find_by_title(tag.attr.delete('title'))
     attrs = {}
-    necessary_attrs = tag.attr.delete('route_handler_params')
-    if Object.const_defined?("RouteHandlerExtension") && tag.locals.page.route_handler_params && necessary_attrs
-      necessary_attrs = necessary_attrs.split(",").map {|p| p.strip.to_sym}
-      tag.locals.page.route_handler_params.each do |key, value| 
-        attrs[key] = value if necessary_attrs.include?(key)
-      end
+    if Object.const_defined?("RouteHandlerExtension") && tag.locals.page.route_handler_params
+      attrs.merge!(tag.locals.page.route_handler_params)
     end
     attrs.merge!(tag.attr)
     if webservice
